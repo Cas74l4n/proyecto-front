ca01644541e78a0d668301eec7e68fb1943a8d90
caste <castelan.15@outlook.es>
2023-12-13 12:15:14 -0600

    solo el menu flata, footer y el modal

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
             slide.style.display = i === index ? "block" : "none";
         });
     }
-
     function nextSlide() {
         currentSlide = (currentSlide + 1) % totalSlides;
         showSlide(currentSlide);
@@ -19,23 +18,18 @@ document.addEventListener("DOMContentLoaded", function () {
         showSlide(currentSlide);
     }
 
-    // Initial display
     showSlide(currentSlide);
 
-    // Auto-advance to the next slide every 3000 milliseconds (3 seconds)
     const autoAdvance = setInterval(nextSlide, 8000);
 
-    // Pause auto-advance on hover
     document.querySelector(".slider-container").addEventListener("mouseenter", function () {
         clearInterval(autoAdvance);
     });
 
-    // Resume auto-advance on mouse leave
     document.querySelector(".slider-container").addEventListener("mouseleave", function () {
         autoAdvance = setInterval(nextSlide, 8000);
     });
 
-    // Navigation arrow click events
     document.querySelector(".prev-arrow").addEventListener("click", function () {
         prevSlide();
     });
@@ -53,3 +47,79 @@ function toggleCard() {
     article.classList.toggle('expanded');
 }
 
+/*boton up  */
+/* document.getElementById("button-up").addEventListener("click", scrollUp)
+
+function scrollUp() {
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0) {
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo(0, currentScroll - (currentScroll / 10));
+    }
+}
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function () {
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 100) {
+        buttonUp.style.transform = "scale(1)";
+    } else if (scroll < 100) {
+        buttonUp.style.transform = "scale(0)";
+    }
+} */
+
+/*boton up  */
+document.addEventListener('DOMContentLoaded', function () {
+    var buttonUp = document.querySelector('.button-up');
+
+    buttonUp.addEventListener('click', function () {
+        scrollToTop(500);
+    });
+
+    function scrollToTop(duration) {
+        var start = document.documentElement.scrollTop;
+        var startTime = null;
+
+        function animateScroll(currentTime) {
+            if (startTime === null) {
+                startTime = currentTime;
+            }
+
+            var elapsedTime = currentTime - startTime;
+            var progress = Math.min(elapsedTime / duration, 1);
+
+            document.documentElement.scrollTop = start * (1 - progress);
+
+            if (progress < 1) {
+                requestAnimationFrame(animateScroll);
+            }
+        }
+
+        requestAnimationFrame(animateScroll);
+    }
+
+    window.onscroll = function () {
+        var scroll = document.documentElement.scrollTop;
+        buttonUp.style.transform = scroll > 100 ? 'scale(1)' : 'scale(0)';
+    };
+});
+
+
+alert('Please wait');
+
+/* Menu */
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector("nav");
+
+menuIcon.addEventListener("click", function () {
+    nav.classList.toggle("active");
+    menuIcon.classList.toggle("active");
+});
+
+window.addEventListener("resize", function () {
+    if (window.innerWidth > 430) {
+        nav.classList.remove("active");
+        menuIcon.classList.remove("active");
+    }
+});
